880710098bbabb4241679e2c64cd5bda1fb000e5
María Andrea Copca <93905665+MariaAndreaCHM@users.noreply.github.com>
2023-10-10 17:27:35 -0600

    Add files via upload

---
+++ Reportes/src/modules/user/entities/person.ts
@@ -0,0 +1,9 @@
+import { Entity, TStatus } from "../../../kernel/types";
+
+export type TPerson = Entity<number> & {
+    username: string;
+    password?: string;
+    status?: TStatus;
+    person?: any;
+    
+};
